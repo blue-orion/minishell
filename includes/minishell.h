ea767346466bced031d38d384a755078e9472f84
blue-orion <xogh080907@gmail.com>
2025-01-15 23:55:23 +0900

    header

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,9 +1,9 @@
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdlib.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif
