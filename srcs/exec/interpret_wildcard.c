4561041e3d994a1170cadfbf8bb02d12da955520
UnivTaeho <xogh080907@gmail.com>
2025-02-24 00:49:00 +0900

    modify interpret wildcard, but have to rewrite the code

--- srcs/exec/interpret_wildcard.c
+++ srcs/exec/interpret_wildcard.c
@@ -6,7 +6,7 @@
 /*   By: takwak <takwak@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:46:41 by takwak            #+#    #+#             */
-/*   Updated: 2025/02/22 18:46:32 by root             ###   ########.fr       */
+/*   Updated: 2025/02/23 22:56:13 by takwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@
 int		is_wildcard(char *text);
 char	*expand_wildcard(DIR *dirp, char *word);
 int		find_match_str(char *match[2], char *word);
+void	copy_wildcard_result(t_data *data, char **result);
 
 int	interpret_wildcard(t_cmd *info, t_data *data)
 {
 	char	**words;
-	char	*path;
+	char	*cur_path;
 	char	*past;
 	DIR		*dirp;
 	int		i;
 
-	path = NULL;
-	path = getcwd(path, 4096);
+	cur_path = NULL;
+	cur_path = getcwd(cur_path, 4096);
 	words = ft_split(data->text, ' ');
 	i = 0;
 	while (words[i])
 	{
 		if (is_wildcard(words[i]))
 		{
-			dirp = opendir(path);
+			dirp = opendir(cur_path);
 			past = words[i];
 			words[i] = expand_wildcard(dirp, words[i]);
 			if (past != words[i])
@@ -44,18 +45,28 @@ int	interpret_wildcard(t_cmd *info, t_data *data)
 		i++;
 	}
 	free(data->text);
+	copy_wildcard_result(data, words);
+	free_pptr((void **)words);
+	free(cur_path);
+	return (0);
+}
+
+void	copy_wildcard_result(t_data *data, char **result)
+{
+	int	i;
+
 	i = 0;
-	while (words[i])
+
+	while (result[i])
 	{
 		if (i == 0)
-			data->text = ft_strdup(words[i]);
+			data->text = ft_strdup(result[i]);
 		else
-			data->text = ft_join_free(data->text, words[i]);
+			data->text = ft_join_free(data->text, result[i]);
 		i++;
+		if (!data->text)
+			error_exit("malloc failed in interpret wildcard");
 	}
-	free(path);
-	free_pptr((void **)words);
-	return (0);
 }
 
 int	is_wildcard(char *text)
@@ -77,31 +88,15 @@ char	*expand_wildcard(DIR *dirp, char *word)
 	struct dirent	*entry;
 	char	*match[2];
 	char	*tmp;
-	int		m_flag[2];
-	int		m_len;
-	int		s_len;
 
 	entry = readdir(dirp);
 	tmp = NULL;
 	find_match_str(match, word);
 	while (entry)
 	{
-		m_flag[0] = 1;
-		m_flag[1] = 1;
-		if (match[0])
-		{
-			m_len = ft_strlen(match[0]);
-			if (ft_strncmp(entry->d_name, match[0], m_len))
-				m_flag[0] = 0;
-		}
-		if (match[1])
-		{
-			m_len = ft_strlen(match[1]);
-			s_len = ft_strlen(entry->d_name);
-			if (ft_strncmp(&entry->d_name[s_len - m_len], match[1], m_len))
-				m_flag[1] = 0;
-		}
-		if (m_flag[0] && m_flag[1] && entry->d_name[0] != '.')
+		if (match_prefix(entry->d_name, match[0])
+			&& match_suffix(entry->d_name, match[1])
+			&& entry->d_name[0] != '.')
 		{
 			if (!tmp)
 				tmp = ft_strdup(entry->d_name);
@@ -118,7 +113,7 @@ char	*expand_wildcard(DIR *dirp, char *word)
 
 int	find_match_str(char *match[2], char *word)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (word[i])
