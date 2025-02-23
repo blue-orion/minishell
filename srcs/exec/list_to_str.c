88bb184d760c63121d9d266dfb957d17a49b5557
UnivTaeho <xogh080907@gmail.com>
2025-02-24 00:48:08 +0900

    repactoring interpret_env, check memory leak and norm

--- srcs/exec/list_to_str.c
+++ srcs/exec/list_to_str.c
@@ -96,8 +96,13 @@ void	interpret_env_all(t_list *head, t_cmd *info)
 	while (head)
 	{
 		data = (t_data *)head->content;
+		if (data->type == SINGLE_QUOTE || !ft_strchr(data->text, '$'))
+		{
+			head = head->next;
+			continue ;
+		}
 		past = data->text;
-		data->text = interpret_env(data->text, data->type, info);
+		data->text = interpret_env(data->text, info);
 		free(past);
 		head = head->next;
 	}
